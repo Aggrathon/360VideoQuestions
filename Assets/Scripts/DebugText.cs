1e11bee13ae4ca2d8020fd6cb2e559d7763ac757
Aggrathon <antonbjo@gmail.com>
2017-01-13 00:45:05 +0200

    Explore filesystems

---
+++ Assets/Scripts/DebugText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class DebugText : MonoBehaviour {
+
+	private static DebugText instance;
+	private Text text;
+
+	void Awake()
+	{
+		text = GetComponent<Text>();
+		instance = this;
+	}
+
+	static public void Log(string str)
+	{
+		if(instance != null)
+			instance.text.text += "\n\n"+ str;
+	}
+
+	void OnDestroy()
+	{
+		if (instance == this)
+			instance = null;
+	}
+}
---
+++ Assets/Scripts/FileExplorer.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class FileExplorer : MonoBehaviour {
+
+	const string FOLDER_NAME = "360VideoQuestions";
+
+	void Start () {
+		VRSettings.enabled = false;
+		Screen.orientation = ScreenOrientation.Portrait;
+		DebugText.Log("Unity External: " + Application.persistentDataPath);
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+		using (AndroidJavaClass jc = new AndroidJavaClass("android.os.Environment"))
+		{
+			string docs = jc.GetStatic<string>("DIRECTORY_DOCUMENTS");
+			string root = jc.CallStatic<AndroidJavaObject>("getExternalStoragePublicDirectory", docs).Call<string>("getPath");
+			DebugText.Log("Android Enviroment Documents: " + root);
+			try
+			{
+				string folder = Path.Combine(root, FOLDER_NAME);
+				if (Directory.CreateDirectory(folder).Exists)
+				{
+					DebugText.Log("Data folder created at: " + folder);
+				}
+			}
+			catch (System.Exception e)
+			{
+				DebugText.Log("<color=red>Could not create directory</color>");
+			}
+		}
+#endif
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.Escape))
+			Application.Quit();
+	}
+}
