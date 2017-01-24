69517cad50ac83a7552485ae89c487a413a7585d
Aggrathon <antonbjo@gmail.com>
2017-01-24 13:15:51 +0200

    VR Button

---
+++ Assets/Scripts/GazeButton.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class GazeButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IPointerClickHandler, IDeselectHandler, ISubmitHandler {
+
+	public float activationTime = 3f;
+	public Image progressbar;
+
+	private float progress = 0f;
+	private bool progressing = false;
+
+	void OnEnable()
+	{
+		progress = 0f;
+		progressing = false;
+	}
+
+	public void OnDeselect(BaseEventData eventData)
+	{
+		progressing = false;
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (EventSystem.current.currentSelectedGameObject == gameObject)
+		{
+			Activate();
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		EventSystem.current.SetSelectedGameObject(gameObject);
+		progressing = true;
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		progressing = false;
+	}
+
+	public void OnSelect(BaseEventData eventData)
+	{
+		progressing = true;
+	}
+
+	public void OnSubmit(BaseEventData eventData)
+	{
+		if (EventSystem.current.currentSelectedGameObject == gameObject)
+		{
+			Activate();
+		}
+	}
+
+	void Update ()
+	{
+		if (progressing)
+		{
+			progress += Time.deltaTime / activationTime;
+			if(progress > 1f)
+			{
+				Activate();
+			}
+			else
+			{
+				progressbar.fillAmount = progress;
+			}
+		}
+		else if(progress != 0f)
+		{
+			progress -= Time.deltaTime / activationTime * 2f;
+			if (progress < 0f)
+			{
+				progress = 0f;
+			}
+			progressbar.fillAmount = progress;
+		}
+	}
+
+	void Activate()
+	{
+		Debug.Log("Activation");
+		progress = 1f;
+		progressbar.fillAmount = progress;
+		progressing = false;
+	}
+}
