7eb95f195a4dacc64cb1b2bb0340e4b6ce776bb2
Aggrathon <antonbjo@gmail.com>
2017-02-12 16:44:20 +0200

    Fix UI looking activation in Cardboard

--- Assets/EditorCameraMovement.cs
+++ Assets/EditorCameraMovement.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.VR;
 
-public class CameraDebug : MonoBehaviour {
-	
+public class EditorCameraMovement : MonoBehaviour {
+
+#if UNITY_EDITOR
 	void Update () {
 		if(Input.GetKey(KeyCode.LeftAlt))
 		{
-			Vector3 rot = transform.parent.eulerAngles;
+			Vector3 rot = transform.eulerAngles;
 			rot.x -= Input.GetAxis("Mouse Y");
 			rot.y += Input.GetAxis("Mouse X");
-			transform.parent.eulerAngles = rot;
+			transform.eulerAngles = rot;
 		}
 	}
+#endif
+
 }
