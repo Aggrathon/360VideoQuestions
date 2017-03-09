c43bc3c909e99cec25b81c4ada086b0756e4687e
Aggrathon <antonbjo@gmail.com>
2017-03-09 22:31:52 +0200

    Refactor Bluetooth java plugin

--- JavaPlugin/plugin/src/main/java/aggrathon/vq360/javaplugin/Bluetooth.java
+++ JavaPlugin/plugin/src/main/java/aggrathon/vq360/javaplugin/Bluetooth.java
@@ -1,86 +1,217 @@
 package aggrathon.vq360.javaplugin;
 
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
 
-public class Bluetooth extends UnityFragment {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Bluetooth {
 
 	public static final java.util.UUID UUID = java.util.UUID.fromString("9968f876-9d9a-48eb-8867-bbec1af63084");
-	private static final int REQUEST_BLUETOOTH_ON = 226;
-	private static final int REQUEST_BLUETOOTH_DISCOVER = 4355;
+	public static final String TAG = "vq360_BLUETOOTH_COM";
 
-	public static void SetEnabled(Activity activity, String unityObject, String unityMethod) {
-		Bluetooth bt = new Bluetooth();
-		bt.setDiscoverable = false;
-		CallFragment(bt, activity, unityObject, unityMethod);
-	}
 
-	public static void SetDiscoverable(Activity activity, String unityObject, String unityMethod) {
-		Bluetooth bt = new Bluetooth();
-		bt.setDiscoverable = true;
-		CallFragment(bt, activity, unityObject, unityMethod);
-	}
+	BluetoothAdapter mBluetoothAdapter;
+	private BluetoothServerSocket mServerSocket;
+	private BluetoothSocket mSocket;
+	private InputStream mInStream;
+	private OutputStream mOutStream;
+	private byte[] mBuffer;
 
-	public static void OpenSettings(Activity activity) {
-		Intent intentOpenBluetoothSettings = new Intent();
-		intentOpenBluetoothSettings.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
-		activity.startActivity(intentOpenBluetoothSettings);
+
+	public Bluetooth() {
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 	}
 
 
-	private boolean setDiscoverable;
+	/**
+	 * Wait for another device to connect
+	 * This is a blocking call (should be called from a thread)
+	 * @return
+	 */
+	public boolean ConnectServer() {
+		Close();
+		if(!mBluetoothAdapter.isEnabled())
+			return false;
+		try {
+			mServerSocket = mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(TAG, UUID);
+		} catch (IOException e) {
+			Log.e(TAG, "Socket's listen() method failed", e);
+		}
+		if (mServerSocket == null)
+			return false;
+		try {
+			if (SetSocket(mServerSocket.accept())) {
+				try {
+					mServerSocket.close();
+				} catch (IOException e) {
+					Log.e(TAG, "Could not close the server socket", e);
+				}
+				mServerSocket = null;
+				return true;
+			}
+		} catch (IOException e) {
+			Log.e(TAG, "Socket's accept() method failed", e);
+		}
+		Close();
+		return false;
+	}
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (mBluetoothAdapter == null) {
-			Callback("Device does not support Bluetooth");
-			CleanUp();
+
+	/**
+	 * Get All Paired BluetoothSettings Devices
+	 * @return csv of devices
+	 */
+	public String GetDevices() {
+		StringBuilder sb = new StringBuilder();
+		for (BluetoothDevice d : mBluetoothAdapter.getBondedDevices()) {
+			sb.append(d.getName().replace(',', '.'));
+			sb.append(',');
+		}
+		if(sb.length() > 0) {
+			return sb.substring(0, sb.length()-1);
 		}
 		else {
-			if (setDiscoverable) {
-				Intent discoverableIntent =	new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-				discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
-				startActivityForResult(discoverableIntent, REQUEST_BLUETOOTH_DISCOVER);
-			}
-			else {
-				if (!mBluetoothAdapter.isEnabled()) {
-					Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-					startActivityForResult(enableBtIntent, REQUEST_BLUETOOTH_ON);
-				}
-				else {
-					Callback("true");
-					CleanUp();
-				}
-			}
-
+			return "";
 		}
 	}
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if(requestCode == REQUEST_BLUETOOTH_DISCOVER) {
-			if (resultCode != Activity.RESULT_CANCELED) {
-				Callback("true");
+
+	/**
+	 * Connect to another device
+	 * @param device
+	 * @return was the connection successful
+	 */
+	public boolean ConnectClient(String device) {
+		for (BluetoothDevice d : mBluetoothAdapter.getBondedDevices()) {
+			if(d.getName().replace(',', '.').equals(device)) {
+				return ConnectClient(d);
 			}
-			else {
-				Callback("false");
-			}
-			CleanUp();
 		}
-		else if(requestCode == REQUEST_BLUETOOTH_ON) {
-			if (resultCode == Activity.RESULT_OK) {
-				Callback("true");
-			}
-			else {
-				Callback("false");
-			}
-			CleanUp();
+		return false;
+	}
+
+
+	/**
+	 * Connect to another device
+	 * @param device
+	 * @return was the connection successful
+	 */
+	public boolean ConnectClient(BluetoothDevice device) {
+		Close();
+		if(!mBluetoothAdapter.isEnabled())
+			return false;
+		try {
+			return SetSocket(device.createRfcommSocketToServiceRecord(UUID));
+		} catch (IOException e) {
+			Log.e(TAG, "Socket's create() method failed", e);
+			return false;
 		}
-		else
-			super.onActivityResult(requestCode, resultCode, data);
+	}
+
+
+	/**
+	 * Sets the communication socket and opens io-streams
+	 * @param socket
+	 * @return operation successful
+	 */
+	private boolean SetSocket(BluetoothSocket socket) {
+		if(socket == null)
+			return false;
+		mSocket = socket;
+		try {
+			mSocket.connect();
+			try {
+				mInStream = mSocket.getInputStream();
+			} catch (IOException e) {
+				Log.e(TAG, "Error occurred when creating input stream", e);
+				Close();
+				return false;
+			}
+			try {
+				mOutStream = mSocket.getOutputStream();
+			} catch (IOException e) {
+				Log.e(TAG, "Could not get the output stream", e);
+				Close();
+				return false;
+			}
+		} catch (IOException connectException) {
+			Log.e(TAG, "Unable to connect", connectException);
+			Close();
+			return false;
+		}
+		if(mBuffer == null) {
+			mBuffer = new byte[1024];
+		}
+		return true;
+	}
+
+
+	/**
+	 * Get Message from the connected device
+	 * This is a blocking call (should be called from a thread)
+	 * The devices should be connected before using this
+	 * @return the message or empty on fail
+	 */
+	public String Listen() {
+		if (mInStream != null) {
+			int numBytes;
+
+			try {
+				numBytes = mInStream.read(mBuffer);
+				return new String(mBuffer, 0, numBytes);
+			} catch (IOException e) {
+				Log.d(TAG, "Input stream was disconnected", e);
+				return "";
+			}
+		}
+		return "";
+	}
+
+
+	/**
+	 * Send a message to the connected device
+	 * The devices should be connected before using this
+	 * @param data the message to send
+	 */
+	public void Send(String data) {
+		if(mOutStream != null) {
+			try {
+				mOutStream.write(data.getBytes());
+			} catch (IOException e) {
+				Log.e(TAG, "Error occurred when sending data", e);
+			}
+		}
+	}
+
+
+	/**
+	 * Close all connections between the devices
+	 */
+	public void Close() {
+		if (mServerSocket != null) {
+			try {
+				mServerSocket.close();
+				mServerSocket = null;
+			} catch (IOException e) {
+				Log.e(TAG, "Could not close the server socket", e);
+			}
+		}
+		if (mSocket != null) {
+			try {
+				mSocket.close();
+				mSocket = null;
+			} catch (IOException e) {
+				Log.e(TAG, "Could not close the communication socket", e);
+			}
+		}
+		mInStream = null;
+		mOutStream = null;
 	}
 }
---
+++ JavaPlugin/plugin/src/main/java/aggrathon/vq360/javaplugin/BluetoothSettings.java
@@ -0,0 +1,85 @@
+package aggrathon.vq360.javaplugin;
+
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+
+public class BluetoothSettings extends UnityFragment {
+
+	private static final int REQUEST_BLUETOOTH_ON = 226;
+	private static final int REQUEST_BLUETOOTH_DISCOVER = 4355;
+
+	public static void SetEnabled(Activity activity, String unityObject, String unityMethod) {
+		BluetoothSettings bt = new BluetoothSettings();
+		bt.setDiscoverable = false;
+		CallFragment(bt, activity, unityObject, unityMethod);
+	}
+
+	public static void SetDiscoverable(Activity activity, String unityObject, String unityMethod) {
+		BluetoothSettings bt = new BluetoothSettings();
+		bt.setDiscoverable = true;
+		CallFragment(bt, activity, unityObject, unityMethod);
+	}
+
+	public static void OpenSettings(Activity activity) {
+		Intent intentOpenBluetoothSettings = new Intent();
+		intentOpenBluetoothSettings.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+		activity.startActivity(intentOpenBluetoothSettings);
+	}
+
+
+	private boolean setDiscoverable;
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			Callback("Device does not support BluetoothSettings");
+			CleanUp();
+		}
+		else {
+			if (setDiscoverable) {
+				Intent discoverableIntent =	new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+				discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
+				startActivityForResult(discoverableIntent, REQUEST_BLUETOOTH_DISCOVER);
+			}
+			else {
+				if (!mBluetoothAdapter.isEnabled()) {
+					Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+					startActivityForResult(enableBtIntent, REQUEST_BLUETOOTH_ON);
+				}
+				else {
+					Callback("true");
+					CleanUp();
+				}
+			}
+
+		}
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == REQUEST_BLUETOOTH_DISCOVER) {
+			if (resultCode != Activity.RESULT_CANCELED) {
+				Callback("true");
+			}
+			else {
+				Callback("false");
+			}
+			CleanUp();
+		}
+		else if(requestCode == REQUEST_BLUETOOTH_ON) {
+			if (resultCode == Activity.RESULT_OK) {
+				Callback("true");
+			}
+			else {
+				Callback("false");
+			}
+			CleanUp();
+		}
+		else
+			super.onActivityResult(requestCode, resultCode, data);
+	}
+}
